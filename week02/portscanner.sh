2554149b2a11563d781d70682c8032ca2279134c
Kyle <healykys@gmail.com>
2019-01-24 21:04:26 -0600

    Added variable timeout with -t

--- week02/portscanner.sh
+++ week02/portscanner.sh
@@ -5,19 +5,9 @@
 # display a usage message if incorrect number of arguments
 function usage
 {
-	echo "Args = $#"
-	if [[ "$#" -ne 0 ]] && [[ "$#" -ne 2 ]] && [[ "$#" -ne 3 ]] && [[ "$#" -ne 5 ]]
-	then
-		echo "Usage: ./portscanner.sh [-t timeout] [<host> <startport> <stopport>]"
-		exit
-	fi
+	echo "Usage: ./portscanner.sh [-t timeout] [<host> <startport> <stopport>]"
+	exit
 }
-usage
-
-# populate our variables from the arguments.
-host=$1
-startport=$2
-stopport=$3
 
 # function pingcheck
 # ping a device to see if it is up
@@ -39,7 +29,7 @@ function portcheck
 {
 	for ((counter=$startport; counter <= $stopport; counter++))
 	do
-		if timeout 2 bash -c "echo > /dev/tcp/$host/$counter"
+		if timeout $timeoutVal bash -c "echo > /dev/tcp/$host/$counter"
 		then
 			echo "Port $counter open"
 		else
@@ -48,6 +38,34 @@ function portcheck
 #		(echo >/dev/tcp/$host/$counter) > /dev/null 2>%1 && echo "$counter open"
 	done
 }
+
+
+# populate our variables from the arguments.
+# Check if optional -t exists
+if [[ "$#" == 5 ]] && [[ "$1" == "-t" ]]
+then
+	timeoutVal=$2
+	host=$3
+	startport=$4
+	stopport=$5
+elif [[ "$#" == 3 ]]
+then
+	timeoutVal=2
+	host=$1
+	startport=$2
+	stopport=$3
+elif [[ "$#" == 2 ]] && [[ "$1" == "-t" ]]
+then
+	timeoutVal=$2
+	#Batch Mode
+elif [[ "$#" == 0 ]]
+then
+	timeoutVal=2
+	#Batch Mode
+else
+	usage
+fi
+
 
 # run our functions
 pingcheck
